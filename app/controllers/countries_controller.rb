ef1e121e815ba78cda18daabfd755910a86d6d28
lechodiman <lechodiman@uc.cl>
2018-08-27 22:27:28 -0300

    Change rubocop configuration

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,56 +2,51 @@ class CountriesController < ApplicationController
     before_action :find_country, only: [:show, :edit, :update, :destroy]
     before_action :authenticate_user!, only: [:new, :edit]
 
+    def index
+        @countries = Country.all.order('created_at DESC')
+    end
 
-	def index
-		@countries = Country.all.order("created_at DESC")
-	end 
+    def show
+    end
 
-	def show
-		
-	end
+    def new
+        @country = Country.new
+    end
 
-	def new
-		@country = Country.new
-	end
+    def create
+        @country = Country.new(country_params)
 
-	def create
-	   @country = Country.new(country_params)
-
-		if @country.save
-			flash[:success] = "Country created successfully!"
-			redirect_to root_path
-		else
-			render 'new'
-		end
-	end
-
-	def edit
-		
-	end
-
-	def update
-		if @country.update(country_params)
-			redirect_to country_path(@country)
-		else
-			render 'edit'
-		end
-	end
-
-	def destroy
-		@country.destroy
-		redirect_to root_path
-	end
-
-	private
-
-		def country_params
-			params.require(:country).permit(:name, :description)
-		end
-
-        def find_country
-            @country = Country.find(params[:id])            
+        if @country.save
+            flash[:success] = "Country created successfully!"
+            redirect_to root_path
+        else
+            render 'new'
         end
+    end
 
+    def edit
+    end
 
+    def update
+        if @country.update(country_params)
+            redirect_to country_path(@country)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @country.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def country_params
+        params.require(:country).permit(:name, :description)
+    end
+
+    def find_country
+        @country = Country.find(params[:id])
+    end
 end
