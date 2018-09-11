166f8a6615ea9a826c7863903ae00024d07eff5a
nlparraguez@uc.cl <you@example.com>
2018-09-11 12:21:10 -0300

    create hotels crud,new,show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
     
+  get 'hotels/index'
+
+  get 'hotels/new'
+
+  get 'hotels/show'
+
     devise_for :users
     
     resources :countries do
-        resources :cities
+        resources :cities do 
+            resources :hotels do
+            	
+            end
+        	
+        end
     end
 
     root 'countries#index'
