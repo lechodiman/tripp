45f894b926395a1f3067c25d190b40f0991e85b1
lechodiman <lechodiman@uc.cl>
2018-09-13 11:12:39 -0300

    :sparkles: Added user gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+  def avatar
+    email_address = self.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+
+    # compile URL which can be used in <img src="RIGHT_HERE"...
+    image_src = "https://www.gravatar.com/avatar/#{hash}"  
+  end
+
 end
