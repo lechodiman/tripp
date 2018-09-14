3ecbebd917f154845b86c8c44f2b557e80bb36ea
nlparraguez@uc.cl <you@example.com>
2018-09-14 17:42:58 -0300

    post layout done

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :edit]
+
+
+  def index
+  	@posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	    @post = Post.new(post_params)
+        
+        if @post.save
+            flash[:success] = "Post created successfully!"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  	@post.destroy
+  	redirect_to root_path
+  end
+
+  def update
+  	    if @post.update(post_params)
+            redirect_to post_path(@post)
+        else
+            render 'edit'
+        end
+
+
+  end
+
+   private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
+    def find_post
+        @post = Post.find(params[:id])
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
