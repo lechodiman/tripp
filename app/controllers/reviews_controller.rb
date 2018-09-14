fd9200a8cfb27ffa01c9ab82553d7f842589ed1a
lechodiman <lechodiman@uc.cl>
2018-09-14 18:40:27 -0300

    Delete and update review of hotels

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
     before_action :authenticate_user!
+    before_action :find_review, only: [:show, :edit, :update, :destroy]
 
     def create
         @review = @reviewable.reviews.new(review_params)
@@ -9,9 +10,22 @@ class ReviewsController < ApplicationController
         redirect_to root_path, notice: "Your comment was successfully posted"
     end
 
+    def destroy
+        @review.destroy
+        redirect_to root_path
+    end
+
+    def edit
+        
+    end
+
     private
         def review_params
             params.require(:review).permit(:body)
+        end
+
+        def find_review
+            @review = Review.find(params[:id])
         end
 
 end
