a6f09d2c1c60710ddb526cd6a605b263f55b1338
Luis Loyola <llloyola@uc.cl>
2018-10-07 17:45:05 -0300

    fix validations for citywalks

--- app/models/citywalk.rb
+++ app/models/citywalk.rb
@@ -3,9 +3,8 @@ class Citywalk < ApplicationRecord
     has_many :reviews, as: :reviewable
 
     VALID_TEXT_REGEX = /\A[a-zA-Z ]+\z/
-    NON_EMPTY_REGEX = /\A[.]+\z/
-  	validates :name, format: { with: VALID_TEXT_REGEX }, uniqueness: { case_sensitive: false }
-  	validates :name, format: { with: NON_EMPTY_REGEX }, uniqueness: { case_sensitive: false }
-  	validates :name, format: { with: VALID_TEXT_REGEX }, uniqueness: { case_sensitive: false }
+  	validates :name, presence: true, format: { with: VALID_TEXT_REGEX }, uniqueness: { case_sensitive: false }
+  	validates :description, presence: true
+  	validates :place, presence: true, format: { with: VALID_TEXT_REGEX }, uniqueness: { case_sensitive: false }
 
 end
