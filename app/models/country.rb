070141e065f4b2615ade2df28a153ad7edbbafad
Luis Loyola <llloyola@uc.cl>
2018-10-07 17:17:44 -0300

    add regex validation for country name

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,9 @@
 class Country < ApplicationRecord
     has_many :cities
 
+    VALID_USERNAME_REGEX = /\A[a-zA-Z ]+\z/
+  	validates :name, presence: true, format: { with: VALID_USERNAME_REGEX }, uniqueness: { case_sensitive: false }
+  	validates :description, presence: true
+
     resourcify
 end
