f1ed84e676108d60fb25092c81921ec928128066
Luis Loyola <llloyola@uc.cl>
2018-10-07 17:37:41 -0300

    add validations for citywalks

--- app/models/citywalk.rb
+++ app/models/citywalk.rb
@@ -2,4 +2,10 @@ class Citywalk < ApplicationRecord
 	belongs_to :city
     has_many :reviews, as: :reviewable
 
+    VALID_TEXT_REGEX = /\A[a-zA-Z ]+\z/
+    NON_EMPTY_REGEX = /\A[.]+\z/
+  	validates :name, format: { with: VALID_TEXT_REGEX }, uniqueness: { case_sensitive: false }
+  	validates :name, format: { with: NON_EMPTY_REGEX }, uniqueness: { case_sensitive: false }
+  	validates :name, format: { with: VALID_TEXT_REGEX }, uniqueness: { case_sensitive: false }
+
 end
