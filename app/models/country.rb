6a19689a9badb3fe819b49e419f4a1b96b115adf
Luis Loyola <llloyola@uc.cl>
2018-10-07 17:47:56 -0300

    fix regex name

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,8 +1,8 @@
 class Country < ApplicationRecord
     has_many :cities
 
-    VALID_USERNAME_REGEX = /\A[a-zA-Z ]+\z/
-  	validates :name, presence: true, format: { with: VALID_USERNAME_REGEX }, uniqueness: { case_sensitive: false }
+    VALID_NAME_REGEX = /\A[a-zA-Z ]+\z/
+  	validates :name, presence: true, format: { with: VALID_NAME_REGEX }, uniqueness: { case_sensitive: false }
   	validates :description, presence: true
 
     resourcify
