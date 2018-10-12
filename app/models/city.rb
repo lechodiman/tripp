9213f753c7696de2a49da54ba67a178397bce2e5
nlparraguez@uc.cl <you@example.com>
2018-10-12 12:47:14 -0300

    fixed controller of comment and city

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,6 +2,7 @@ class City < ApplicationRecord
     belongs_to :country
     has_many :hotels
     has_many :citywalks
+    has_many :restaurants
     has_many :reviews, as: :reviewable
     mount_uploader :image, ImageUploader
 end
