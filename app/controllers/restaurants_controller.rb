19520bf0521ed7cad7a8957b9da340d581302bc6
Luis Loyola <llloyola@uc.cl>
2018-10-13 18:50:22 -0300

    clean file

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,55 +1,52 @@
 class RestaurantsController < ApplicationController
-  before_action :find_city, only: [:new, :create]
-  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit]
-  
-  def index
-  end
+    before_action :find_city, only: [:new, :create]
+    before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:new, :edit]
 
-  def new
-  	@restaurant = Restaurant.new
-  end
-
-  def update
-    if @restaurant.update(restaurants_params)
-      redirect_to restaurant_path(@restaurant)
-    else
-      render 'edit'
-    end    
-  end
-
-  def destroy
-    this_city = @restaurant.city
-    @restaurant.destroy
-    redirect_to city_path(this_city)
-  end
-
-  def create
-    @restaurant = Restaurant.new(restaurants_params)
-    @restaurant.city_id = @city.id
-
-    if @restaurant.save
-        flash[:success] = "restaurant created successfully!"
-        redirect_to city_path(@city)
-    else
-        render 'new'
+    def index
     end
-  end
 
+    def new
+        @restaurant = Restaurant.new
+    end
 
-
-
-
-  private
-  		def restaurants_params
-            params.require(:restaurant).permit(:name, :description)
+    def update
+        if @restaurant.update(restaurants_params)
+            redirect_to restaurant_path(@restaurant)
+        else
+            render 'edit'
         end
+    end
 
-        def find_city
-            @city = City.find(params[:city_id])
-        end
+    def destroy
+        this_city = @restaurant.city
+        @restaurant.destroy
+        redirect_to city_path(this_city)
+    end
 
-        def find_restaurant
-            @restaurant = Restaurant.find(params[:id])
+    def create
+        @restaurant = Restaurant.new(restaurants_params)
+        @restaurant.city_id = @city.id
+
+        if @restaurant.save
+            flash[:success] = 'restaurant created successfully!'
+            redirect_to city_path(@city)
+        else
+            render 'new'
         end
+    end
+
+    private
+
+    def restaurants_params
+        params.require(:restaurant).permit(:name, :description)
+    end
+
+    def find_city
+        @city = City.find(params[:city_id])
+    end
+
+    def find_restaurant
+        @restaurant = Restaurant.find(params[:id])
+    end
 end
