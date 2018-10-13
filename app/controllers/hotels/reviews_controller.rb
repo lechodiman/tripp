39b3695c66a38accf86a55ebbc11bdb066c35980
Luis Loyola <llloyola@uc.cl>
2018-10-13 19:29:26 -0300

    clean file

--- app/controllers/hotels/reviews_controller.rb
+++ app/controllers/hotels/reviews_controller.rb
@@ -2,8 +2,8 @@ class Hotels::ReviewsController < ReviewsController
     before_action :set_reviewable, only: [:new, :create]
 
     private
-        def set_reviewable
-            @reviewable = Hotel.find(params[:hotel_id])
-        end
 
+    def set_reviewable
+        @reviewable = Hotel.find(params[:hotel_id])
+    end
 end
