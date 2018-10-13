55a5db606331d498ebed51248a3eb92299d45133
Luis Loyola <llloyola@uc.cl>
2018-10-13 19:32:18 -0300

    clean file

--- app/controllers/restaurants/reviews_controller.rb
+++ app/controllers/restaurants/reviews_controller.rb
@@ -2,8 +2,8 @@ class Restaurants::ReviewsController < ReviewsController
     before_action :set_reviewable, only: [:new, :create]
 
     private
-        def set_reviewable
-            @reviewable = Restaurant.find(params[:restaurant_id])
-        end
 
+    def set_reviewable
+        @reviewable = Restaurant.find(params[:restaurant_id])
+    end
 end
