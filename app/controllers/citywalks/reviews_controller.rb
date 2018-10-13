f5c8ba937faf90b5815c29e442fcb079494f9153
Luis Loyola <llloyola@uc.cl>
2018-10-13 18:55:43 -0300

    clean file

--- app/controllers/citywalks/reviews_controller.rb
+++ app/controllers/citywalks/reviews_controller.rb
@@ -2,8 +2,8 @@ class Citywalks::ReviewsController < ReviewsController
     before_action :set_reviewable, only: [:new, :create]
 
     private
-        def set_reviewable
-            @reviewable = Citywalk.find(params[:citywalk_id])
-        end
 
+    def set_reviewable
+        @reviewable = Citywalk.find(params[:citywalk_id])
+    end
 end
