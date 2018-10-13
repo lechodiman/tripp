8b7aadd19cca9409c416bf361ccdcb3920903ae4
Luis Loyola <llloyola@uc.cl>
2018-10-13 18:24:39 -0300

    clean file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
-  def index
-  end
+    def index
+    end
 
-  def show
-  end
+    def show
+    end
 
-  def new
-  end
+    def new
+    end
 end
