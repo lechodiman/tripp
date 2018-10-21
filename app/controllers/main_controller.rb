2b0c15003c3e2b9f37e28b457d1a04b4f2f6ec00
lechodiman <lechodiman@uc.cl>
2018-10-21 13:14:30 -0300

    :sparkles: global search for countries, cities, restaurants and hotels

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,19 @@ class MainController < ApplicationController
     end
 
     def search
-        render json: { countries: [], cities: [] }
+        @countries = Country.ransack(name_cont: params[:q]).result(distinct: true)
+        @cities = City.ransack(name_cont: params[:q]).result(distinct: true)
+        @hotels = Hotel.ransack(name_cont: params[:q]).result(distinct: true)
+        @restaurants = Restaurant.ransack(name_cont: params[:q]).result(distinct: true)
+
+        respond_to do |format|
+            format.html {}
+            format.json {
+                @countries = @countries.limit(5)
+                @cities = @cities.limit(5)
+                @hotels = @hotels.limit(5)
+                @restaurants = @restaurants.limit(5)
+            }
+        end
     end
 end
