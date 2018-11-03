03dc2a7cac0787a5dbf209f292d5f0c69e85e37d
lechodiman <lechodiman@uc.cl>
2018-11-03 12:50:29 -0300

    :bug: fix  poll vote create

--- app/controllers/poll_votes_controller.rb
+++ app/controllers/poll_votes_controller.rb
@@ -1,11 +1,13 @@
 class PollVotesController < ApplicationController
     def create
+        puts "HOLAAAAZAAAA"
+
         if current_user && params[:poll] && params[:poll][:id] && params[:vote_option] && params[:vote_option][:id]
             @poll = Poll.find(params[:poll][:id])
             @option = @poll.vote_options.find(params[:vote_option][:id])
     
             if @option && @poll && !current_user.voted_for?(@poll)
-                @options.poll_votes.create({user_id: current_user.id})
+                @option.poll_votes.create({user_id: current_user.id})
             else
                 render js: 'alert(\'Your vote cannot be saved. Have you already voted?\');'
             end
