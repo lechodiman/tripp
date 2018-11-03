53b7e5fdf8a6966b61cbd6cfd770308e57692192
Luis Loyola <llloyola@uc.cl>
2018-11-03 20:13:22 -0300

    lots of changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20181103164657) do
+ActiveRecord::Schema.define(version: 20181103211206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,6 +139,7 @@ ActiveRecord::Schema.define(version: 20181103164657) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "country_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
