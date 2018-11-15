bf50c8ae3cfb34ddc534445d72228f18ed77374d
Luis Chodiman <lechodiman@uc.cl>
2018-11-15 12:17:52 -0300

    remove title from comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181103211206) do
+ActiveRecord::Schema.define(version: 20181115151655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20181103211206) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
