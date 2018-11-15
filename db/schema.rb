2bef019bc881ffe46fdc26e5e1d0b9bcb47e9436
nlparraguez@uc.cl <you@example.com>
2018-11-15 11:48:26 -0300

    citywalks images ready

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181115135826) do
+ActiveRecord::Schema.define(version: 20181115143020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20181115135826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "city_id"
+    t.string "image"
   end
 
   create_table "comments", force: :cascade do |t|
