0371e8e39f7dc47e931e17faaff970b27875e13d
nlparraguez@uc.cl <you@example.com>
2018-11-15 12:03:17 -0300

    restaurants ready

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -41,7 +41,7 @@ class RestaurantsController < ApplicationController
     private
 
     def restaurants_params
-        params.require(:restaurant).permit(:name, :description)
+        params.require(:restaurant).permit(:name, :description, :image)
     end
 
     def find_city
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181115143020) do
+ActiveRecord::Schema.define(version: 20181115145150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20181115143020) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "city_id"
+    t.string "image"
   end
 
   create_table "reviews", force: :cascade do |t|
