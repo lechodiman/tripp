fa93cc0e51dde726cabc43ee297af5db97fd0c5b
Luis Loyola <llloyola@uc.cl>
2018-11-22 01:31:33 -0300

    add helper to get next 5 days of the week

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,2 +1,37 @@
 module CitiesHelper
+	def next_five_days
+		require 'date'
+
+		current_time = Time.now
+		year = current_time.year
+		month = current_time.month
+		day = current_time.day
+
+		date = Date.new(year, month, day)
+
+		['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+		if date.monday?
+			['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+		elsif date.tuesday?
+			['Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+		elsif date.wednesday?
+			['Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday']
+
+		elsif date.thursday?
+			['Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday']
+
+		elsif date.friday?
+			['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday']
+
+		elsif date.saturday?
+			['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']
+
+		else
+			['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+		end
+	end
 end
