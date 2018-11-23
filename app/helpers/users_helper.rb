3649e6a97258f88b593f9394b0dd643e880e1d9a
lechodiman <lechodiman@uc.cl>
2018-11-23 17:14:13 -0300

    :bug: fix hotels show when user not logged in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,9 @@
 module UsersHelper
     def can_moderate_review(review)
-        if current_user.country_id
-
+        if current_user && current_user.country_id
             review.reviewable.city.country.id == current_user.country_id
-
         else
-
             false
-
         end
     end
 end
